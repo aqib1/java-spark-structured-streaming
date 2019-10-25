2946e188cf843fd3eec09081f8ea6e7151955f96
Aqib Javed <aqibbutt3078@gmail.com>
2019-10-25 13:15:07 +0500

    init main

--- Spark-Kafka/src/main/java/com/spark/kafka/task/SparkKafkaApplication.java
+++ Spark-Kafka/src/main/java/com/spark/kafka/task/SparkKafkaApplication.java
@@ -1,9 +1,11 @@
 package com.spark.kafka.task;
 
+import com.spark.kafka.task.client.SSClient;
+
 public class SparkKafkaApplication {
 
 	public static void main(String[] args) {
-		
+		SSClient.getInstance().initSpark().loadDataFromKafka().writeData();
 	}
 
 }
